6bf26bc8599d78ded8b49b7ce602e41e8b20589c
Pako <pakounicredit@gmail.com>
2022-11-13 18:42:35 +0100

    debug

--- Todo/settings.py
+++ Todo/settings.py
@@ -19,6 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
+DEBUG = False
 ALLOWED_HOSTS = ['p-task.up.railway.app', '127.0.0.1',]
 
 CSRF_TRUSTED_ORIGINS = ['https://p-task.up.railway.app' , ]
